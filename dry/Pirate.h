aee198300ddfd06b07478fa3b7537d1d7b496564
HssainAzaqy135 <kinghssain135@gmail.com>
2024-12-19 18:04:27 +0200

    Update Pirate.h to fix compilation error

--- dry/Pirate.h
+++ dry/Pirate.h
@@ -5,17 +5,22 @@
 
 using std::string;
 
-class Pirate {
+class Pirate
+{
 private:
     string name;
+    int bounty;
 
 public:
-    Pirate(const string& name);
+    Pirate() : name(""), bounty(0) {} // To fix compilation error
+    Pirate(const string &name, const int &bounty) : name(name), bounty(bounty) {}
+    Pirate(const string &name);
 
     ~Pirate() = default;
 
-    void setName(const string& name);
-
+    void setName(const string &name);
+    void setBounty(const int &bounty);
+    int getBounty();
     string getName();
 
     friend std::ostream &operator<<(std::ostream &os, const Pirate &pirate);
